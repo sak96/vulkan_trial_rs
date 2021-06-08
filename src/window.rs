1e1e2f146d400801f116b111764fe1263d62a9d0
sak96 <>
2021-06-08 20:51:02 +0530

    Add the title to the windows

--- src/window.rs
+++ src/window.rs
@@ -11,6 +11,7 @@ use winit::window::WindowBuilder;
 pub fn init_window(instance: &Arc<Instance>) -> (EventLoop<()>, Arc<Surface<Window>>) {
     let event_loop = EventLoop::new();
     let surface = WindowBuilder::new()
+        .with_title(env!("CARGO_PKG_NAME"))
         .build_vk_surface(&event_loop, instance.clone())
         .expect("failed to crate window builder");
     (event_loop, surface)
